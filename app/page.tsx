20257d699a6360b117afc7300a4dfa4ca1bbb4e3
Manas Ranjan Adha <manas865873@gmail.com>
2025-05-30 11:32:36 +0530

    setup shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <h1>Cospendo Home</h1>
+      <Button className="p-2 cursor-pointer">Shadcn</Button>
     </div>
   );
 }
